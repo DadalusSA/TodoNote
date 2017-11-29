020a8a88866d0396b7b7cb245f6febd1dfd05a29
DadalusSA <34018630+DadalusSA@users.noreply.github.com>
2017-11-29 20:51:16 +0800

    Add files via upload

---
+++ js/item-ajax.js
@@ -0,0 +1,112 @@
+﻿$(document).ready(function () {
+    getPageData();
+
+    /* Get Page Data*/
+    function getPageData() {
+        $.ajax({
+            type: 'GET',
+            dataType: 'json',
+            url: url+ 'user/getuserdata.php',
+        }).done(function (data) {
+            manageRow(data.data);
+
+        });
+    }
+
+    /* Add new Item table row */
+    function manageRow(data) {
+        var rows = '';
+        var checkedverify= "checked";
+        $.each(data, function (key, value) {
+            var newchecked = value.Completed;
+            rows = rows + '<tr>';
+            rows = rows + '<td>' + value.Todo + '</td>';
+            rows = rows + '<td data-id="' + value.ID + '">';
+            if (newchecked == 1)
+                {
+                rows = rows + '<input type="checkbox" style="width:21px;height: 21px;" class="getcheck" id="' + value.Todo + '"' + checkedverify + ' />&nbsp&nbsp';
+            }
+            else
+            {
+                rows = rows + '<input type="checkbox" style="width:21px;height: 21px;" class="getcheck" id="' + value.Todo + '" />&nbsp&nbsp';
+            }
+            rows = rows + '<button class="btn btn-danger delete_todo" style="margin-bottom:10px">Del</button>';
+            rows = rows + '</td>';
+            rows = rows + '</tr>';
+            function markdone(getvalue) {
+                alert(getvalue);
+            }
+        });
+
+        $("tbody").html(rows);
+        
+        
+    }
+    $("body").on("click", ".getcheck", function () {
+        var Todo = $(this).parent("td").prev("td").text();
+        var ischecked = $(this).is(":checked");
+            
+            if (ischecked) {
+                $.ajax({
+                    type: 'POST',
+                    url: url +'user/markdone.php',
+                    data: { Todo },
+                }).done(function (getdata) {
+                    alert("Added")
+                });
+            }
+           
+        if (!ischecked) {
+            $.ajax({
+                type: 'POST',
+                url: url + 'user/markincom.php',
+                data: { Todo },
+            }).done(function (getdata2) {
+                alert("Deleted")
+            });
+        }
+    });
+        
+
+        /* Create Todo*/
+         $(".Create_Todoclass").click(function (e) {
+            e.preventDefault();
+            var form_action = $("#createtodo").find("form").attr("action");
+            var getTodo = $("#createtodo").find("input[name='Todo']").val();
+
+            if (getTodo != '') {
+                $.ajax({
+                    dataType: 'json',
+                    type: 'POST',
+                    url: url + form_action,
+                    data: {getTodo},
+                }).done(function (getvalue3) {
+                    $("#createtodo").find("input[name='Todo']").val('');
+                    getPageData();
+                    $(".modal").modal('hide');
+                    toastr.success('Todo Created Successfully.', 'Success Alert', { timeOut: 5000 });
+                });
+            } else {
+                alert('You are missing title or description.')
+            }
+
+        });
+
+        /* Remove Item */
+        $("body").on("click", ".delete_todo", function () {
+            var id = $(this).parent("td").data('id');
+            var clearline= $(this).parents("tr");
+            $.ajax({
+                type: 'POST',
+                url: 'user/userdelete.php',
+                data: { id },
+            }).done(function (data) {
+                clearline.remove();
+                toastr.success('Todo Deleted Successfully.', 'Success Alert', { timeOut: 5000 });
+                getPageData();
+            });
+
+        });
+
+       
+        });
---
+++ js/note-ajax.js
@@ -0,0 +1,119 @@
+﻿$(document).ready(function () {
+    getPageData();
+
+    /* Get Page Data*/
+    function getPageData() {
+        $.ajax({
+            type: 'GET',
+            dataType: 'json',
+            url: url + 'user/getusernote.php',
+        }).done(function (data) {
+            manageRow(data.data);
+
+        });
+    }
+
+    /* Add new Item table row */
+    function manageRow(data) {
+        var rows = '';
+        $.each(data, function (key, value) {
+            rows = rows + '<tr>';
+            rows = rows + '<td>' + value.Title + '</td>';
+            rows = rows + '<td>' + value.Description + '</td>';
+            rows = rows + '<td data-id="' + value.ID + '">';
+            rows = rows + '<button data-toggle="modal" data-target="#editnote" class="btn btn-primary editnotecls">Edit</button> ';
+            rows = rows + '<button class="btn btn-danger delete_note">Del</button>';
+            rows = rows + '</td>';
+            rows = rows + '</tr>';
+            function markdone(getvalue) {
+                alert(getvalue);
+            }
+        });
+
+        $("tbody").html(rows);
+
+
+    }
+   
+
+    /* Create Note*/
+    $(".Create_Noteclass").click(function (e) {
+        e.preventDefault();
+        var form_action = $("#createnote").find("form").attr("action");
+        var getTitle = $("#createnote").find("input[name='title']").val();
+        var getDescription = $("#createnote").find("textarea[name='description']").val();
+
+        if (getTitle != '' && getDescription != '') {
+            $.ajax({
+                dataType: 'json',
+                type: 'POST',
+                url: url + form_action,
+                data: { getTitle,getDescription },
+            }).done(function (getvalue4) {
+                $("#createnote").find("input[name='title']").val('');
+                $("#createnote").find("textarea[name='description']").val('');
+                getPageData();
+                $(".modal").modal('hide');
+                toastr.success('Note Successfully Added.', 'Success Alert', { timeOut: 5000 });
+            });
+        } else {
+            alert('You are missing title or description.')
+        }
+
+    });
+
+    /* Remove Item */
+    $("body").on("click", ".delete_note", function () {
+        var id = $(this).parent("td").data('id');
+        var clearline = $(this).parents("tr");
+        $.ajax({
+            type: 'POST',
+            url: url + 'user/userdeletenote.php',
+            data: { id },
+        }).done(function (data) {
+            clearline.remove();
+            toastr.success('Note Deleted Successfully.', 'Success Alert', { timeOut: 5000 });
+            getPageData();
+        });
+
+    });
+
+    /* Edit Item */
+    $("body").on("click", ".editnotecls", function () {
+
+        var id = $(this).parent("td").data('id');
+        var description = $(this).parent("td").prev("td").text();
+        var title = $(this).parent("td").prev("td").prev("td").text();
+        
+
+        $("#editnote").find("input[name='title']").val(title);
+        $("#editnote").find("textarea[name='description']").val(description);
+        $("#editnote").find(".edit-id").val(id);
+
+    });
+
+    /* Updated new Item */
+    $(".submitedit").click(function (ev) {
+        ev.preventDefault();
+        var form_action = $("#editnote").find("form").attr("action");
+        var getTitle = $("#editnote").find("input[name='title']").val();
+        var getDescription = $("#editnote").find("textarea[name='description']").val();
+        var id = $("#editnote").find(".edit-id").val();
+
+        if (getTitle != '' && getDescription != '') {
+            $.ajax({
+                dataType: 'json',
+                type: 'POST',
+                url: url + form_action,
+                data: { getTitle, getDescription, id }
+            }).done(function (data) {
+                getPageData();
+                $(".modal").modal('hide');
+                toastr.success('Note Changed.', 'Success Alert', { timeOut: 5000 });
+
+            });
+        } else {
+            alert('You are missing title or description.')
+        }
+    });
+});
